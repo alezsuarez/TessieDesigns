83015f729406a07eb8c39c2ce11d4235a399ff24
alezsuarez <alinsure92@gmail.com>
2019-12-03 17:32:34 -0600

    Primer commit de tessie designs

---
+++ app/src/main/java/com/example/tessiedesign/TableActivity.java
@@ -0,0 +1,13 @@
+package com.example.tessiedesign;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TableActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
